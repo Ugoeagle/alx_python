c6df966402d03d46014bb184faf7dd08de951c30
(Ugoeagle) <(uchechukwun10@gmail)>
2023-12-05 21:15:46 +0100

    noi

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -6,34 +6,6 @@ elif number ==0:
 else:
     print(number, "is negative")
     
-pass
-
-number = -98
-if number < -100:
-    print(number, "is positive")
-elif number ==0:
-    print(number, "is zero")
-else:
-    print(number, "is negative")
-    
-    pass
-
-number = 0
-if number > 0:
-    print(number, "is positive")
-elif number ==0:
-    print(number, "is zero")
-else:
-    print(number, "is negative")
-    
-    pass
-number = 10
-if number > -0:
-    print(number, "is positive")
-elif number ==0:
-    print(number, "is zero")
-else:
-    print(number, "is negative")
     
       
     
