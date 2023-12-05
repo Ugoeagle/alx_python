b18f4b22079c9f5798ab786a3bcdee6852727380
(Ugoeagle) <(uchechukwun10@gmail)>
2023-12-05 21:30:56 +0100

    ndo

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,15 +1,32 @@
-# number >98
-# if number > 0:
- #print(number, "is positive")
-#elif number ==0:
-  #  print(number, "is zero")
-#else:
-   # print(number, "is negative")
-    
+number = 98
+if number < -100:
+    print(number, "is positive")
+elif number ==0:
+    print(number, "is zero")
+else:
+    print(number, "is negative")
 # 2nd positive
-
 number = -98
 if number < -100:
+    print(number, "is positive")
+elif number ==0:
+    print(number, "is zero")
+else:
+    print(number, "is negative")
+    
+    pass
+
+number = 0
+if number > 0:
+    print(number, "is positive")
+elif number ==0:
+    print(number, "is zero")
+else:
+    print(number, "is negative")
+    
+    pass
+number = 10 + hiss
+if number > -0:
     print(number, "is positive")
 elif number ==0:
     print(number, "is zero")
