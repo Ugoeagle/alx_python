6f0ff56b01dff34c1b900873f61a8392ec337f47
(Ugoeagle) <(uchechukwun10@gmail)>
2023-12-05 21:21:32 +0100

    bea

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,10 +1,21 @@
-number = 98
-if number > 0:
+# number >98
+# if number > 0:
+ #print(number, "is positive")
+#elif number ==0:
+  #  print(number, "is zero")
+#else:
+   # print(number, "is negative")
+    
+# 2nd positive
+
+number = -98
+if number < -100:
     print(number, "is positive")
 elif number ==0:
     print(number, "is zero")
 else:
     print(number, "is negative")
+
     
     
       
