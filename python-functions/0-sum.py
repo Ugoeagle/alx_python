26285da45e5fba0f1201bbf87f44f584d8da5f9f
(Ugoeagle) <(uchechukwun10@gmail)>
2024-01-07 08:32:03 +0100

    hjuk

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,5 +1,15 @@
+# sum.py no2
 def add(a, b):
     return  a + b
 print(add(1, 2))
+
+# sum.py no2
+def add(a, b):
+    return  a + b
 print(add(98, 0))
+
+#sum.py n03
+def add(a, b):
+    return  a + b
 print(add(100, -2))
+
