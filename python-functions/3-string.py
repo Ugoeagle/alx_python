b22164d5f9dcfeb806365991a8759b8954838cc3
(Ugoeagle) <(uchechukwun10@gmail)>
2024-01-07 13:05:18 +0100

    string

---
+++ python-functions/3-string.py
@@ -0,0 +1,7 @@
+def reverse_string(input_string):
+    reversed_string = input_string[::-1]
+    return reversed_string
+print(reverse_string("Hello"))
+print(reverse_string(""))
+print(reverse_string("madam"))
+print(reverse_string("Hello, World!"))
